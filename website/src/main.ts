d6b2a167b7e71c17f0d9abe1bbe6aa29873f9a02
Jovica <jovica.d@northriverproject.com>
2026-01-30 13:00:31 +0100

    move the folders

--- website/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import VueOnboardingTour from 'vue-onboarding-tour'
-import { createApp } from 'vue'
-import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fas)
-createApp(App).use(VueOnboardingTour).mount('#app')
--- website/tailwind.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-}
